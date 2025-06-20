d1664038f31c138c218afe1cca032eb677645892
pipoo34 <belouahrighassane97@gmail.com>
2025-06-20 18:19:41 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
-export default ({ env }) => ({
+// server.ts with TypeScript fixes
+export default ({ env }: { env: any }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
