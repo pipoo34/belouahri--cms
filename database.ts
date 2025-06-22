6001c0beb11c0a2ee042312e43b57fc48108bfae
pipoo34 <belouahrighassane97@gmail.com>
2025-06-22 22:29:02 +0100

    Update database.ts

--- database.ts
+++ database.ts
@@ -1,9 +1,23 @@
 // config/database.ts
-export default ({ env }) => ({
+import path from 'path';
+
+interface EnvironmentFunctions {
+  (key: string, defaultValue?: any): any;
+  int(key: string, defaultValue?: number): number;
+  bool(key: string, defaultValue?: boolean): boolean;
+  array(key: string, defaultValue?: any[]): any[];
+}
+
+export default ({ env }: { env: EnvironmentFunctions }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      filename: path.join(
+        __dirname,
+        '..',
+        '..',
+        env('DATABASE_FILENAME', '.tmp/data.db')
+      ),
     },
     useNullAsDefault: true,
   },
